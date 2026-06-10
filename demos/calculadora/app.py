4e32c817ef3cd05192d206109cf415e7719fd43e
ERICK MONTIEL GARCIA <1725110382@utectulancingo.edu.mx>
2026-06-10 14:08:16 +0000

    UPDATED calculadora

--- demos/calculadora/app.py
+++ demos/calculadora/app.py
@@ -15,6 +15,16 @@ class Index:
 class Calculadora:
     def GET(self):
         return render.calculadora()
+    
+    def POST(self):
+        formulario = web.input()
+        numero_1= float(formulario['numero_1'])
+        numero_2= float(formulario['numero_2'])
+        resultado = numero_1 + numero_2
 
+        print(f"Tipo de dato de numero_1: {type(numero_1)}")
+        print(f"Tipo de dato de numero_2: {type(numero_2)}")
+
+        return f"La suma de {numero_1} + {numero_2} = {resultado}"
 if __name__ == "__main__":
     app.run()
